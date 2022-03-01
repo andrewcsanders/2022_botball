2b2969a16c135298a299a7a335ad5a31963d0721
andrewcsanders <100621749+andrewcsanders@users.noreply.github.com>
2022-03-01 13:27:21 -0600

    Update mic.h

--- mic.h
+++ mic.h
@@ -1,3 +1,12 @@
 
-int setup();
-int shutdown();
+int setup(int full);
+void shutdown();
+
+
+int ABORT;
+
+float ET[74];
+float rotate_modif[72];
+float neg_rotate_modif[72];
+float slope_interp(float per,float arr[]);
+void servo_hdl();
